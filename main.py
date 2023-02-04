789ee8074443fbf859144d7b936788472f280563
UlkuTuncerKucuktas <ulku.tuncer.kucuktas@gmail.com>
2023-02-04 16:55:42 +0300

    fix

--- main.py
+++ main.py
@@ -15,29 +15,33 @@ def add_a_day(date):
     return date
 
 
-
-def get_tweets(search_term, start_date, end_date,number_of_tweets):
-
+def get_tweets(search_term, start_date, end_date, number_of_tweets, save_file):
     """
     This function will return a list of tweets that match the search term
     :param search_term: The term to search for
     :param start_date: The start date of the search
     :param end_date: The end date of the search
     :param number_of_tweets: The number of tweets to return
-    :return: dataframe of tweets
+    :param save_file: the name of the file to save the tweets to
+    :return: None
     """
-    tweets_list = []
+    # Initialize the dataframe and write the header to the csv file
+    tweets_df = pd.DataFrame(columns=["User", "Date Created", "Number of Likes", "Source of Tweet", "Tweet"])
+    tweets_df.to_csv(save_file + ".csv", mode='w', index=False)
+
     current_date = start_date
     while current_date != end_date:
-        for i, tweet in enumerate(sntwitter.TwitterSearchScraper(search_term + ' since:' + current_date + ' until:' + add_a_day(current_date)).get_items()):
+        for i, tweet in enumerate(sntwitter.TwitterSearchScraper(search_term + ' since:' + current_date + ' until:' + add_a_day(current_date) +' lang:tr').get_items()):
             if i > number_of_tweets:
                 break
-            tweets_list.append([tweet.user.username, tweet.date, tweet.likeCount, tweet.source, tweet.rawContent])
+
+            # Append each tweet to the csv file
+            tweet_data = pd.DataFrame([[tweet.user.username, tweet.date, tweet.likeCount, tweet.source, tweet.rawContent]], columns=["User", "Date Created", "Number of Likes", "Source of Tweet", "Tweet"])
+            tweet_data.to_csv(save_file + ".csv", mode='a', index=False, header=False)
+
         current_date = add_a_day(current_date)
-    tweets_df = pd.DataFrame(tweets_list, columns=["User", "Date Created", "Number of Likes", "Source of Tweet", "Tweet"])
-    print(tweets_df.head(10))
-        
-    return tweets_df
+    print("Done")
+
 
 if __name__ == '__main__':
     search_term = input("Enter the search term: ")
@@ -45,13 +49,4 @@ if __name__ == '__main__':
     end_date = input("Enter the end date in format Year-Month-Day: ")
     number_of_tweets = int(input("Enter the number of tweets per day: "))
     save_file = input("Enter the name of the file to save the tweets to: ")
-    tweets_df = get_tweets(search_term, start_date, end_date, number_of_tweets)
-    tweets_df.to_csv(save_file + ".csv", index=False)
-    print("Done")
-
-
-
-
-
-
-
+    get_tweets(search_term, start_date, end_date, number_of_tweets, save_file)
